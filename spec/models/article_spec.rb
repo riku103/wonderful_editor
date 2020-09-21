8cd4e0a84d7672ce3b6cbaf6d25ecb91c70cbe74
新田陸人 <>
2020-09-21 12:17:27 +0900

    テスト実装のリファクタリング

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,33 +2,32 @@ require "rails_helper"
 
 RSpec.describe Article, type: :model do
   context "必要な情報が入力されているとき" do
+    let(:user) { create(:user) }
+    let(:article) { build(:article, user_id: user.id) }
     it "記事が登録できる" do
-      user = create(:user)
-      article = build(:article, user_id: user.id)
       expect(article).to be_valid
     end
   end
 
   context "title がない場合" do
+    let(:user) { create(:user) }
+    let(:article) { build(:article, title: nil, user_id: user.id) }
     it "エラーが発生する" do
-      user = create(:user)
-      article = build(:article, title: nil, user_id: user.id)
       expect(article).not_to be_valid
-      expect(article.errors.details[:title][0][:error]).to eq :blank
     end
   end
 
   context "ユーザー登録していない場合" do
+    let(:article) { build(:article) }
     it "エラーが発生する" do
-      article = build(:article)
       expect(article).not_to be_valid
     end
   end
 
   context "body が10文字以下の場合" do
-    fit "エラーが発生する" do
-      user = create(:user)
-      article = build(:article, body: "テスト", user_id: user.id)
+    let(:user) { create(:user) }
+    let(:article) { build(:article, body: "テスト", user_id: user.id) }
+    it "エラーが発生する" do
       expect(article).not_to be_valid
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   context "必要な情報が揃っている場合" do
@@ -36,12 +36,10 @@ RSpec.describe User, type: :model do
   end
 
   context "既に同じ name が存在している場合" do
-    # let(:user) { create(:user, name: "riku") }
     it "エラーが発生する" do
       create(:user, name: "riku")
-      user= build(:user, name: "riku")
+      user = build(:user, name: "riku")
       expect(user).not_to be_valid
-      expect(user.errors.details[:name][0][:error]).to eq :taken
     end
   end
 end
